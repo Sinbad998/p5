c85ac1b68923c93f221f8fc060060175d476630c
younes oublal <oublal.youness72@gmail.com>
2025-01-15 12:18:10 +0100

    p5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,34 @@
-import Card from "../components/Card"
-import picture from '../assets/sourceHome.png'
-import Datas from "../utils/Datas.json"
+import { useState, useEffect } from "react";
+import Card from "../components/Card";
+import picture from "../assets/sourceHome.png";
+import Datas from "../utils/Datas.json";
 
-const Home = ()=>{
-  return(
+const Home = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 500);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 500);
+    };
+
+    window.addEventListener("resize", handleResize);
+  } );
+
+  return (
     <main className="home">
       <div className="banner">
         <img src={picture} alt="Montagnes avec titre de page" />
-        <h1>Chez vous,partout et ailleurs</h1>
+        <h1>
+          Chez vous,
+          {isSmallScreen ? <br /> : " "}
+          partout et ailleurs
+        </h1>
       </div>
       <div className="gallery">
-        <Card Datas={Datas}/>
+        <Card Datas={Datas} />
       </div>
     </main>
-  )
-}
+  );
+};
 
-
-
-export default Home
+export default Home;
